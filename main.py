386ce84ac2b93ac7fcdd85a5cd640f5c893ae467
Manish Raje <manishbraje@gmail.com>
2026-05-30 13:48:57 +0530

    set up inngest and now doing vector db thing

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ import os
 import datetime
 from inngest.experimental import ai
 
+
+
 load_dotenv()
 inngest_client = inngest.Inngest(
     app_id="rag_app",
-    logging = logging.getLogger("uvicorn"),
+    logger = logging.getLogger("uvicorn"),
     is_production = False,
     serializer = inngest.PydanticSerializer()
 )
 
-
-
+@inngest_client.create_function(
+    fn_id="RAG : Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def ingest_pdf(ctx: inngest.Context):
+    return {"hello": "world"}
 
 app = FastAPI()
 
 
 
-inngest.fast_api.serve(app, inngest_client,[])
+inngest.fast_api.serve(app, inngest_client, [ingest_pdf])
