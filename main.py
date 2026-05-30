79c01ae7125ffe74650a34665fd60a16206b0ebd
Manish Raje <manishbraje@gmail.com>
2026-05-30 16:55:13 +0530

    added steps in inngest and seeing how it works

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import datetime
 from inngest.experimental import ai
 from data_loader import load_and_chunk_pdf, embed_text
 from vector_db import QdrantStorage
-
+from custom_types import RAGChunkAndSrc, RAGUpsertResult, RAGSearchResult, RAGQueryResult
 
 load_dotenv()
 inngest_client = inngest.Inngest(
@@ -24,7 +24,25 @@ inngest_client = inngest.Inngest(
     trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
 )
 async def ingest_pdf(ctx: inngest.Context):
-    return {"hello": "world"}
+    def _load(ctx: inngest.Context) -> RAGChunkAndSrc:
+        pdf_path = ctx.event.data["pdf_path"]
+        source_id = ctx.event.data.get("source_id", pdf_path)
+        chunks = load_and_chunk_pdf(pdf_path)
+        return RAGChunkAndSrc(chunks=chunks, source_id=source_id)
+
+    def _upsert(chunks_and_src: RAGChunkAndSrc) -> RAGUpsertResult:
+        chunks = chunks_and_src.chunks
+        source_id = chunks_and_src.source_id
+        vecs = embed_text(chunks)
+        ids = [str(uuid.uuid5(uuid.NAMESPACE_URL, f"{source_id}_{i}")) for i in range(len(chunks))]
+        payloads = [{"source": source_id, "text": chunks[i]} for i in range(len(chunks))]
+        QdrantStorage().upsert(ids, vecs, payloads)
+        return RAGUpsertResult(ingested=len(chunks)) 
+
+    chunks_and_src = await ctx.step.run("load-and-chunk", lambda: _load(ctx), output_type = RAGChunkAndSrc)
+    ingested = await ctx.step.run("embed-and-upsert", lambda: _upsert(chunks_and_src), output_type = RAGUpsertResult)
+    return ingested.model_dump()
+
 
 app = FastAPI()
 
